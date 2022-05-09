2958afcd6de7d7d6e80d543f9e8442cc1d7c718d
Aris Vayas <aris.vayas@gmail.com>
2022-05-09 17:49:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState} from 'react';
 
 function App() {
+  const [audioPlayBack, setAudioPlayBack]=useState(true)
+  const audio = new Audio(
+    "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+  );
+
+  const start = () => {
+    audio.play() 
+    setAudioPlayBack(audioPlayBack=>!audioPlayBack);
+  }
+}
+  ;
+const stopaudio =()=>{
+  audio.pause() 
+  audio.currentTime = 0
+   setAudioPlayBack(audioPlayBack=>!audioPlayBack)
+}
+ ;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +36,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+      <button onClick={audioPlayBack ? start:stopaudio}>{audioPlayBack? "Start":"stop"}</button>
+    </div>
       </header>
     </div>
-  );
-}
+  );}
+  
 
 export default App;
