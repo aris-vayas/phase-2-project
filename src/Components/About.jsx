50649419a40d9ff92e73f7ce971d130ba2cd1b94
Aris Vayas <aris.vayas@gmail.com>
2022-05-12 11:39:21 -0400

    cleaned up Extra

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -14,17 +14,42 @@ const About = () => {
           <br></br>
           <br></br>
           <Typography variant="body1">
-          The real name of this music artist is Noah Oliver Smith. He was born on February 26th, 2000 in the city of Irvine, California. Yeat is mixed with Mexican and Romanian, with his father being Mexican and his mother being Romanian. When entering highschool, Yeat moved to Portland Oregon and started his teenage life there. He began making music at a young age, not picking up as many followers as he is now. 
-
-Yeat is a 21 year old underground rapper that has recently begun to blow up in success. Songs such as “sorry bout that ” and “get busy” have picked up a lot of traction on social media apps like tiktok, instagram and more, with people repeatedly using that sound in their own titkok videos. Yeat has gained so much  following that other famous music artists such as Drake have gone out of their way to meet this young man and repost his music to their fanbase. 
-
-Yeat has come out with music since he was 15 year old, with him starting off using his phone. He now has come out with his newest album called “2 Alive” released on February 18. This new album has over 5 million streams on Spotify and keeps going up, gaining new listeners everyday.
-
-“This up and coming music artist brings to the table a new type of music that gets me pumped when I listen to it,” said sophomore Erik Hemby.
-
-Tiktok played a big role in making this new rapper popular. Yeat’s music has sounds and noises in it that draw people to not only listen to it, but make dances and funny videos to it. One popular noise is the bell sound that tends to be in his songs. Another popular song by Yeat that people use on Tiktok is the song, “Sorry About That” with over 12 million listens on spotify and 3+ million on soundcloud. These songs become trends to use on the app with many creative videos coming out as a result of it. One thing that separates Yeat from other rappers is that he releases so much music, he drops music at least every 2 weeks. Many other rappers don’t do this because they think that they won’t get many views/listens when they release an excess of music but Yeat doesn’t believe this and continues to put out music. A 10th grader from Parkdale said that she likes when a good music artist puts out a lot of music it draws me more into the artists and makes me a bigger fan.
-
-Overall, music artist Noah Oliver Smith is a young talent and has much time to grow and be great in the music industry.
+            The real name of this music artist is Noah Oliver Smith. He was born
+            on February 26th, 2000 in the city of Irvine, California. Yeat is
+            mixed with Mexican and Romanian, with his father being Mexican and
+            his mother being Romanian. When entering highschool, Yeat moved to
+            Portland Oregon and started his teenage life there. He began making
+            music at a young age, not picking up as many followers as he is now.
+            Yeat is a 21 year old underground rapper that has recently begun to
+            blow up in success. Songs such as “sorry bout that ” and “get busy”
+            have picked up a lot of traction on social media apps like tiktok,
+            instagram and more, with people repeatedly using that sound in their
+            own titkok videos. Yeat has gained so much following that other
+            famous music artists such as Drake have gone out of their way to
+            meet this young man and repost his music to their fanbase. Yeat has
+            come out with music since he was 15 year old, with him starting off
+            using his phone. He now has come out with his newest album called “2
+            Alive” released on February 18. This new album has over 5 million
+            streams on Spotify and keeps going up, gaining new listeners
+            everyday. “This up and coming music artist brings to the table a new
+            type of music that gets me pumped when I listen to it,” said
+            sophomore Erik Hemby. Tiktok played a big role in making this new
+            rapper popular. Yeat’s music has sounds and noises in it that draw
+            people to not only listen to it, but make dances and funny videos to
+            it. One popular noise is the bell sound that tends to be in his
+            songs. Another popular song by Yeat that people use on Tiktok is the
+            song, “Sorry About That” with over 12 million listens on spotify and
+            3+ million on soundcloud. These songs become trends to use on the
+            app with many creative videos coming out as a result of it. One
+            thing that separates Yeat from other rappers is that he releases so
+            much music, he drops music at least every 2 weeks. Many other
+            rappers don’t do this because they think that they won’t get many
+            views/listens when they release an excess of music but Yeat doesn’t
+            believe this and continues to put out music. A 10th grader from
+            Parkdale said that she likes when a good music artist puts out a lot
+            of music it draws me more into the artists and makes me a bigger
+            fan. Overall, music artist Noah Oliver Smith is a young talent and
+            has much time to grow and be great in the music industry.
           </Typography>
         </div>
       </div>
